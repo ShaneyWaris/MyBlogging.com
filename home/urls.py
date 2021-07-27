1d7414fd64896f931b03620b2c0f39af13be47a7
Shaney Waris <shaney18308@iiitd.ac.in>
2021-07-28 02:47:47 +0530

    added registration

--- home/urls.py
+++ home/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('' , home , name="home"),
-    path('login/' , login_view , name="login_view"),
-    path('register/' , register_view , name="register_view"),
+    path('', home , name="home"),
+    path('login/', login_view , name="login_view"),
+    path('register/', register_view , name="register_view"),
     path('add-blog/' , add_blog, name="add_blog"),
     path('blog-detail/<slug>' , blog_detail , name="blog_detail"),
     path('see-blog/' , see_blog , name="see_blog"),
